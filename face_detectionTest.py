159deace81369f23b2ccc0e9d75a0aa8f335ee88
PTD <ptd4tech@gamil.com>
2025-06-22 23:40:44 +0300

    Added An Fps Counter

--- face_detectionTest.py
+++ face_detectionTest.py
@@ -1,24 +1,34 @@
 import cv2
+import time
 
-# Main Function
 
-def Main ():
-    CameraCapture = cv2.VideoCapture(0)
-    while CameraCapture.isOpened():
-        ret,frame = CameraCapture.read()
-        CameraFeed = frame
-        
-        FlippedFeed = cv2.flip(CameraFeed,1)
-        cv2.imshow("Camera Feed",FlippedFeed)
-        
-        # cv2.imshow("Camera Feed",frame)
-        
-        cv2.waitKey(1)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    CameraCapture.release()
-    cv2.destroyAllWindows()
+def Main():
+  print("Welcome to my App")
+  CameraCapture = cv2.VideoCapture(0)
+  FpsPrevTime = 0
+  
+  while CameraCapture.isOpened() :
+    ret, frame = CameraCapture.read()
+    
+    # Flip The Feed
+    
+    frame = cv2.flip(frame, 1)
+    
+    # Calculates Fps
+    CurrTime = time.time()
+    FPS = 1 / (CurrTime - FpsPrevTime)
+    FpsPrevTime = CurrTime
+    # Display the Fps Counter
+    
+    cv2.putText(frame, f"FPS: {int(FPS)}", (10, 70), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0, 255, 0), 2)
+    
+    cv2.imshow("TestApp",frame)
+    cv2.waitKey(1)
+    
+    
+    
+    # Break Lopp with Q pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+       break
+    
 Main()
-
-
-
